3e712ca02bd933a98c5050ea92c08de017ca5396
Kbrake444 <kbrake444@gmail.com>
2025-02-14 20:17:00 -0700

    circle circumference converter file

---
+++ circle circumference converter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+class Main {
+    
+    private double r;
+    private double c;
+    
+    public void getCirc (double circ){
+    double getCirc = 3.14 * (2 * r);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Enter the Radius: ");
+        double r1 = sc.nextDouble();
+        double getCirc = 3.14 * (2 * r1);
+        System.out.println ("The Circumference is:");
+        System.out.println ("2 * " + r1 + " * 3.14 = " + getCirc);
+    }
+}
+
+//no commit
